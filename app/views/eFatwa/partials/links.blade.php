bcf3eee1ab1a112f34c1144c84eee6f75741c861
j_javadi <j_javadi@yahoo.com>
2016-02-03 22:37:20 +0330

    اضافه کردن تولتیپ

--- app/views/eFatwa/partials/links.blade.php
+++ app/views/eFatwa/partials/links.blade.php
@@ -1,7 +1,7 @@
 
 <div class="rightsectionlinks">
 	<ul>
-		<li><a href="http://lib.eshia.ir" target="_blank">@lang(sprintf('%s/app.feqahat_lib', Config::get('app_settings.theme')))</a></li>
+		<li><a href="http://lib.eshia.ir" target="_blank" title="لینک به کتابخانه مدرسه فقاهت-بخش شیعی">@lang(sprintf('%s/app.feqahat_lib', Config::get('app_settings.theme')))</a></li>
 		
 	</ul>
 </div>
