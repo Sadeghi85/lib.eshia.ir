358d466159ce2a6111c1b7cb4deea11ea1c2d2d4
j_javadi <j_javadi@yahoo.com>
2016-02-03 22:16:59 +0330

    حذف منوهای سمت راست

--- app/views/alfeker/partials/links.blade.php
+++ app/views/alfeker/partials/links.blade.php
@@ -1,23 +1,7 @@
 
 <div class="rightsectionlinks">
 	<ul>
-		<li><a href="http://www.eshia.ir" target="_blank">@lang(sprintf('%s/app.feqahat_school', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://www.eshia.ir/feqh/archive/" target="_blank">@lang(sprintf('%s/app.qom_masjed_e_azam', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://www.eshia.ir/Feqh/Archive/qom/" target="_blank">@lang(sprintf('%s/app.qom', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://www.eshia.ir/Ar/Feqh/Archive/Qom/" target="_blank">@lang(sprintf('%s/app.qom_arabic', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://www.eshia.ir/Ar/Feqh/Archive/" target="_blank">@lang(sprintf('%s/app.najaf', Config::get('app_settings.theme')))</a></li>
-        <li><a href="http://www.eshia.ir/Ar/Feqh/Archive/qaravieh/" target="_blank">@lang(sprintf('%s/app.najaf_algharavieh', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://www.eshia.ir/Feqh/Archive/Mashhad/" target="_blank">@lang(sprintf('%s/app.mashhad', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://www.eshia.ir/Ar/Feqh/Archive/lebanon/" target="_blank">@lang(sprintf('%s/app.lebanon', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://www.eshia.ir/Feqh/Archive/Afghanestan/" target="_blank">@lang(sprintf('%s/app.afghanistan', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://www.eshia.ir/Feqh/Archive/Iran/" target="_blank">@lang(sprintf('%s/app.iran', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://www.eshia.ir/Feqh/Archive/Moaser/" target="_blank">@lang(sprintf('%s/app.moaser', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://www.eshia.ir/Feqh/Archive/Subject/" target="_blank">@lang(sprintf('%s/app.subjects', Config::get('app_settings.theme')))</a></li> 
-		<li><a href="http://www.eshia.ir/Aqaed/Archive/" target="_blank">@lang(sprintf('%s/app.kalam', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://www.eshia.ir/Ar/Feqh/Archive/Translate/" target="_blank">@lang(sprintf('%s/app.taghrir', Config::get('app_settings.theme')))</a></li>
-		<li><hr size="1" color="#3e5061" width="90%" align="right"></li>
-		<li><a href="http://sath3.eshia.ir" target="_blank">@lang(sprintf('%s/app.level_3', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://wikifeqh.ir" target="_blank">@lang(sprintf('%s/app.wiki_feqh', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://wikiporsesh.ir" target="_blank">@lang(sprintf('%s/app.wiki_porsesh', Config::get('app_settings.theme')))</a></li>
+		<li><a href="http://lib.eshia.ir" target="_blank">@lang(sprintf('%s/app.feqahat_lib', Config::get('app_settings.theme')))</a></li>
+		
 	</ul>
 </div>
