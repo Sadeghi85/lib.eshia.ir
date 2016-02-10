6b37b6916412e09cf9c01e755893416c141a806b
Aryan <mahdi.aryan@gmail.com>
2016-02-10 15:00:47 +0330

    جا گذاری کدها در قالب سایت مربوط به صفحه نخست سایت کتابخانه

--- app/views/alfeker/index.blade.php
+++ app/views/alfeker/index.blade.php
@@ -1,17 +1,13 @@
 
 @section('content')
-<div id="contents" class="content-DefaultPage">
-		<table id="content-DefaultPage-Table" cellpadding="0" cellspacing="0">
-			<tr>
-				<td class="DefaultPage-Board">
-					<div class="welcome">
-						<span>@lang(sprintf('%s/app.index_welcome', Config::get('app_settings.theme')))</span>
-						<br />
-						<span>@lang(sprintf('%s/app.books_information', Config::get('app_settings.theme')), array('books_count' => sprintf('<span class="book_Counter_Page1"> %s </span>', $booksCount), 'vols_count' => sprintf('<span class="book_Counter_Page1"> %s </span>', $volsCount)))</span>
-					</div>
-				</td>
-			</tr>
-		</table>    
+
+<div id="contents">
+
+<div class="welcome">
+	<span>@lang(sprintf('%s/app.index_welcome', Config::get('app_settings.theme')))</span>
+	<br />
+	<span>@lang(sprintf('%s/app.books_information', Config::get('app_settings.theme')), array('books_count' => sprintf('<span class="book_Counter_Page1"> %s </span>', $booksCount), 'vols_count' => sprintf('<span class="book_Counter_Page1"> %s </span>', $volsCount)))</span>
+</div>
 </div>
 
 <div class="DefaultPage-down">
@@ -21,6 +17,7 @@
 		<a href="/help">@lang(sprintf('%s/app.index_attention_lib_help', Config::get('app_settings.theme')), array('help' => sprintf('<font color="#FF6633">%s</font>', trans(sprintf('%s/app.lib_help', Config::get('app_settings.theme'))))))</a>
 	</span>
 </div>
+
 @stop
 
 @section('title')
