c9dd3012c01a087ea00db151cd66691b0eeffce5
Aryan <mahdi.aryan@gmail.com>
2016-03-17 10:55:23 +0330

    تنظیمات مربوط به منوی سمت راست کتابخانه اصلی

--- app/views/main_lib/partials/links.blade.php
+++ app/views/main_lib/partials/links.blade.php
@@ -1,23 +1,12 @@
-
 <div class="rightsectionlinks">
 	<ul>
-		<li><a href="http://www.eshia.ir" target="_blank">@lang(sprintf('%s/app.feqahat_school', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://www.eshia.ir/feqh/archive/" target="_blank">@lang(sprintf('%s/app.qom_masjed_e_azam', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://www.eshia.ir/Feqh/Archive/qom/" target="_blank">@lang(sprintf('%s/app.qom', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://www.eshia.ir/Ar/Feqh/Archive/Qom/" target="_blank">@lang(sprintf('%s/app.qom_arabic', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://www.eshia.ir/Ar/Feqh/Archive/" target="_blank">@lang(sprintf('%s/app.najaf', Config::get('app_settings.theme')))</a></li>
-        <li><a href="http://www.eshia.ir/Ar/Feqh/Archive/qaravieh/" target="_blank">@lang(sprintf('%s/app.najaf_algharavieh', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://www.eshia.ir/Feqh/Archive/Mashhad/" target="_blank">@lang(sprintf('%s/app.mashhad', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://www.eshia.ir/Ar/Feqh/Archive/lebanon/" target="_blank">@lang(sprintf('%s/app.lebanon', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://www.eshia.ir/Feqh/Archive/Afghanestan/" target="_blank">@lang(sprintf('%s/app.afghanistan', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://www.eshia.ir/Feqh/Archive/Iran/" target="_blank">@lang(sprintf('%s/app.iran', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://www.eshia.ir/Feqh/Archive/Moaser/" target="_blank">@lang(sprintf('%s/app.moaser', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://www.eshia.ir/Feqh/Archive/Subject/" target="_blank">@lang(sprintf('%s/app.subjects', Config::get('app_settings.theme')))</a></li> 
-		<li><a href="http://www.eshia.ir/Aqaed/Archive/" target="_blank">@lang(sprintf('%s/app.kalam', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://www.eshia.ir/Ar/Feqh/Archive/Translate/" target="_blank">@lang(sprintf('%s/app.taghrir', Config::get('app_settings.theme')))</a></li>
-		<li><hr size="1" color="#3e5061" width="90%" align="right"></li>
-		<li><a href="http://sath3.eshia.ir" target="_blank">@lang(sprintf('%s/app.level_3', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://wikifeqh.ir" target="_blank">@lang(sprintf('%s/app.wiki_feqh', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://wikiporsesh.ir" target="_blank">@lang(sprintf('%s/app.wiki_porsesh', Config::get('app_settings.theme')))</a></li>
+		<li><a href="http://eshia.ir" target="_blank" title="لینک به مدرسه فقاهت">@lang(sprintf('%s/app.feqahat_school', Config::get('app_settings.theme')))</a></li>
+		<li><hr></li>
+		<li><a href="http://alfeker.lib.eshia.ir/" target="_blank" title="لینک به کتابخانه تصویری مدرسه فقاهت">@lang(sprintf('%s/app.feqahatlib_image', Config::get('app_settings.theme')))</a></li>
+		<li><a href="http://lib.fatawa.eshia.ir" target="_blank" title="لینک به کتابخانه مدرسه فقاهت-بخش اهل سنت">@lang(sprintf('%s/app.feqahatlib_fatwa', Config::get('app_settings.theme')))</a></li>
+		<li><a href="http://pdf.lib.eshia.ir/" target="_blank" title="لینک به کتابخانه تصویری مدرسه فقاهت اهل سنت">@lang(sprintf('%s/app.feqahatlib_image_fatwa', Config::get('app_settings.theme')))</a></li>
+		<li><hr></li>
+		<li><a href="http://www.wikifeqh.ir/" target="_blank" title="دانشنامه فقه و اصول و علوم حوزوی">@lang(sprintf('%s/app.wiki_feqh', Config::get('app_settings.theme')))</a></li>
+		<li><a href="http://www.wikiporsesh.ir/" target="_blank" title="دانشنامه پرسش دینی">@lang(sprintf('%s/app.wiki_porsesh', Config::get('app_settings.theme')))</a></li>
 	</ul>
 </div>
