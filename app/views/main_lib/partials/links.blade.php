1c44952534da2c73dcf684c18a227e035f88b738
Aryan <mahdi.aryan@gmail.com>
2016-03-17 13:02:55 +0330

    اصلاح لینک سمت راست کتابخانه ها

--- app/views/main_lib/partials/links.blade.php
+++ app/views/main_lib/partials/links.blade.php
@@ -2,8 +2,8 @@
 	<ul>
 		<li><a href="http://eshia.ir" target="_blank" title="لینک به مدرسه فقاهت">@lang(sprintf('%s/app.feqahat_school', Config::get('app_settings.theme')))</a></li>
 		<li><hr></li>
-		<li><a href="http://alfeker.lib.eshia.ir/" target="_blank" title="لینک به کتابخانه تصویری مدرسه فقاهت">@lang(sprintf('%s/app.feqahatlib_image', Config::get('app_settings.theme')))</a></li>
-		<li><a href="http://lib.fatawa.eshia.ir" target="_blank" title="لینک به کتابخانه مدرسه فقاهت-بخش اهل سنت">@lang(sprintf('%s/app.feqahatlib_fatwa', Config::get('app_settings.theme')))</a></li>
+		<li><a href="http://pdf.lib.eshia.ir/" target="_blank" title="لینک به کتابخانه تصویری مدرسه فقاهت">@lang(sprintf('%s/app.feqahatlib_image', Config::get('app_settings.theme')))</a></li>
+		<li><a href="http://lib.efatwa.ir" target="_blank" title="لینک به کتابخانه مدرسه فقاهت-بخش اهل سنت">@lang(sprintf('%s/app.feqahatlib_fatwa', Config::get('app_settings.theme')))</a></li>
 		<li><a href="http://pdf.lib.efatwa.ir/" target="_blank" title="لینک به کتابخانه تصویری مدرسه فقاهت -اهل سنت">@lang(sprintf('%s/app.feqahatlib_image_fatwa', Config::get('app_settings.theme')))</a></li>
 		<li><hr></li>
 		<li><a href="http://www.wikifeqh.ir/" target="_blank" title="دانشنامه فقه و اصول و علوم حوزوی">@lang(sprintf('%s/app.wiki_feqh', Config::get('app_settings.theme')))</a></li>
