4064884cfeafa11ba907cc22d4e139d2b7928138
Aryan <mahdi.aryan@gmail.com>
2016-03-17 13:50:40 +0330

    یکسان کردن منوی سمت راست کتابخانه ها

--- app/views/main_lib/partials/links.blade.php
+++ app/views/main_lib/partials/links.blade.php
@@ -2,6 +2,7 @@
 	<ul>
 		<li><a href="http://eshia.ir" target="_blank" title="لینک به مدرسه فقاهت">@lang(sprintf('%s/app.feqahat_school', Config::get('app_settings.theme')))</a></li>
 		<li><hr></li>
+		<li><a href="http://lib.eshia.ir/" target="_blank" title="لینک به کتابخانه مدرسه فقاهت">@lang(sprintf('%s/app.feqahat_lib', Config::get('app_settings.theme')))</a></li>		
 		<li><a href="http://pdf.lib.eshia.ir/" target="_blank" title="لینک به کتابخانه تصویری مدرسه فقاهت">@lang(sprintf('%s/app.feqahatlib_image', Config::get('app_settings.theme')))</a></li>
 		<li><a href="http://lib.efatwa.ir" target="_blank" title="لینک به کتابخانه مدرسه فقاهت-بخش اهل سنت">@lang(sprintf('%s/app.feqahatlib_fatwa', Config::get('app_settings.theme')))</a></li>
 		<li><a href="http://pdf.lib.efatwa.ir/" target="_blank" title="لینک به کتابخانه تصویری مدرسه فقاهت -اهل سنت">@lang(sprintf('%s/app.feqahatlib_image_fatwa', Config::get('app_settings.theme')))</a></li>
